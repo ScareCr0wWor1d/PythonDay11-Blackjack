f4b6de233f927d80912f2a3eb810f0e360d5c104
ScareCr0wWor1d <proulx_marc@hotmail.com>
2024-01-14 15:20:42 -0500

    Final

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from art import logo
 def donne_carte():
     """Fonction pour donner une carte"""
     paquet = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    #paquet = [11, 10]
     carte = random.choice(paquet)
     return carte
 
@@ -26,6 +27,10 @@ def calcule_total(carte):
         return 0
     return sum(carte)
 
+def swapace(cartes):
+    cartes.remove(11)
+    cartes.append(1)
+    return cartes
 
 end_of_game = False
 cpu = []
@@ -34,37 +39,62 @@ joueur = []
 print(logo)
 
 while not end_of_game:
+    #Donne des cartes
     for _ in range(2):
         joueur.append(donne_carte())
         cpu.append(donne_carte())
+
+    #Check si Blackjack
+    if calcule_total(joueur) > 21:
+        swapace(joueur)
+    if calcule_total(cpu) > 21:
+        swapace(cpu)
     total_joueur = calcule_total(joueur)
     total_cpu = calcule_total(cpu)
     affiche_carte(1)
-    while input("voulez-vous obtenir une nouvelle carte (o/n)? ") == 'o':
-        system('cls')
-        print(logo)
-        joueur.append(donne_carte())
-        affiche_carte(1)
-        if sum(joueur) > 21:
-            end_of_game = True
-            break
-    while sum(cpu) < 17 and not end_of_game:
-        cpu.append(donne_carte())
-        if sum(cpu) > 21:
-            end_of_game = True
+
+    #check si le joueur veut d'autres cartes
+    if total_joueur != 0:
+        while input("voulez-vous obtenir une nouvelle carte (o/n)? ") == 'o':
+            system('cls')
+            print(logo)
+            joueur.append(donne_carte())
+            if calcule_total(joueur) > 21 and 11 in joueur:
+                joueur = swapace(joueur)
+            affiche_carte(1)
+            if calcule_total(joueur) > 21:
+                end_of_game = True
+                break
+
+    #Ajuste deck cpu if < 17
+    if calcule_total(cpu) != 0:
+        while sum(cpu) < 17 and not end_of_game and calcule_total(cpu) != 0:
+            cpu.append(donne_carte())
+            if calcule_total(cpu) > 21 and 11 in cpu:
+                cpu = swapace(cpu)
+            if sum(cpu) > 21:
+                end_of_game = True
+
+    total_cpu = calcule_total(cpu)
+    total_joueur = calcule_total(joueur)
     system('cls')
     print(logo)
     affiche_carte(2)
-    if end_of_game and sum(cpu) > 21:
-        print("Vous avez gagné! CPU Busted!")
-    elif end_of_game and sum(joueur) > 21:
-        print("CPU a gagné! Joueur Busted!")
-    elif sum(joueur) == sum(cpu):
+
+    if total_joueur == total_cpu:
         print("Partie nulle")
+    elif total_joueur == 0:
+        print("Joueur a BLACKJACK et gagne!")
+    elif total_cpu == 0:
+        print("CPU a BLACKJACK et gagne!")
+    elif total_joueur > 21:
+        print("Joueur BUST, CPU Gagne")
+    elif total_cpu > 21:
+        print("CPU BUST, joueur Gagne")
     elif sum(joueur) > sum(cpu):
-        print("Vous avez gagner!")
+        print("Joueur gagne!")
     else:
-        print("CPU a gagné!")
+        print("CPU gagne!")
 
     if input("Voulez-vous rejouer (o/n)? ") == 'n':
         end_of_game = True
